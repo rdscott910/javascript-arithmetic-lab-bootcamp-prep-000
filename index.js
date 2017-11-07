17b7918ac6598ddc16a375b9cdea5e6f3f79150a
Ronnie Scott <rscott910@outlook.com>
2017-11-07 23:20:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a, b) {
 function divide(a, b) {
   return(a / b)
 }
-function inc(a) {
+function inc(n) {
   return(a++)
 }
-function dec(a) {
+function dec(n) {
   return(a--)
 }
